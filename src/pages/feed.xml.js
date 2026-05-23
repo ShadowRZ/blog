ee932b1a24c8fff7c6f931bca1cd172d5ce4d570
夜坂雅 <23130178+ShadowRZ@users.noreply.github.com>
2026-05-23 15:11:53 +0800

    feat: Add RSS

---
+++ src/pages/feed.xml.js
@@ -0,0 +1,21 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+  const blog = await getCollection('posts');
+
+  return rss({
+    title: "@ShadowRZ's Blog",
+    description: 'Where something happens.',
+    site: context.site,
+    trailingSlash: false,
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      // Compute RSS link from post `id`
+      // This example assumes all posts are rendered as `/blog/[id]` routes
+      link: `/blog/posts/${post.id}`,
+    })),
+  });
+}
